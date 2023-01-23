92b530df5de89a055044441355e119373c18b097
rospi <62301694+venieris95@users.noreply.github.com>
2023-01-23 21:10:10 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,12 +4,7 @@ import re
 from stop_words import stopwords
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
 import streamlit as st
-import pickle
-from pathlib import Path
-import streamlit_authenticator as stauth
 
 
 # REVIEWS
