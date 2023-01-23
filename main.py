68d26d504965048deb9a789e67c746245357b142
rospi <62301694+venieris95@users.noreply.github.com>
2023-01-23 20:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import streamlit as st
 
-def check_password():
+def check_credentials():
     """Returns `True` if the user had a correct password."""
 
-    def password_entered():
+    def credentials_entered():
         """Checks whether a password entered by the user is correct."""
         if (
-            st.session_state["username"] in st.secrets["passwords"]
+            st.session_state["username"] in st.secrets["credentials"]
             and st.session_state["password"]
             == st.secrets["passwords"][st.session_state["username"]]
         ):
-            st.session_state["password_correct"] = True
+            st.session_state["credentials_correct"] = True
             del st.session_state["password"]  # don't store username + password
             del st.session_state["username"]
         else:
-            st.session_state["password_correct"] = False
+            st.session_state["credentials_correct"] = False
 
-    if "password_correct" not in st.session_state:
+    if "credentials_correct" not in st.session_state:
         # First run, show inputs for username + password.
         st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         return False
-    elif not st.session_state["password_correct"]:
+    elif not st.session_state["credentials_correct"]:
         # Password not correct, show input + error.
         st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
@@ -35,6 +35,6 @@ def check_password():
         # Password correct.
         return True
 
-if check_password():
+if check_credentials():
     st.write("Here goes your normal Streamlit app...")
     st.button("Click me")
