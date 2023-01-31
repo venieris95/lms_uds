8d987221b70ef30112937c75d48c609a689decbb
rospi <62301694+venieris95@users.noreply.github.com>
2023-01-31 12:26:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -94,10 +94,41 @@ def main():
                     sns.heatmap(df2.corr(), ax=ax)
                     st.write(fig)
             if graphs == 'Scatter Plot':
-                colors = [plt.cm.tab10(i/float(len(v.courses)-1)) for i in range(len(v.courses))]
-                fig = plt.scatter(v.courses, v.absences)
+                df1 = v.first_semester                    
+                df2 = v.second_semester
+                left_label = [str(c) + ', '+ str(round(y)) for c, y in zip(df1['name'], v.overall_1)]
+                right_label = [str(c) + ', '+ str(round(y)) for c, y in zip(df2['name', v.overall_2)]
+                klass = ['red' if (v.overall_1-v.overall_2) < 0 else 'green']
+                                                                            
+                def newline(p1, p2, color='black'):
+                    ax = plt.gca()
+                    l = mlines.Line2D([p1[0],p2[0]], [p1[1],p2[1]], color='red' if p1[1]-p2[1] > 0 else 'green', marker='o', markersize=6)
+                    ax.add_line(l)
+                    return l
+
+                fig, ax = plt.subplots(1,1,figsize=(14,14), dpi= 80)
+                ax.vlines(x=1, ymin=500, ymax=13000, color='black', alpha=0.7, linewidth=1, linestyles='dotted')
+                ax.vlines(x=3, ymin=500, ymax=13000, color='black', alpha=0.7, linewidth=1, linestyles='dotted')
+                ax.scatter(y=v.overall_1, x=np.repeat(1, df.shape[0]), s=10, color='black', alpha=0.7)
+                ax.scatter(y=v.overall_2, x=np.repeat(3, df.shape[0]), s=10, color='black', alpha=0.7)                                                            
+                for p1, p2, c in zip(v.overall_1, v.overall_2, df1['name']):
+                    newline([1,p1], [3,p2])
+                    ax.text(1-0.05, p1, c + ', ' + str(round(p1)), horizontalalignment='right', verticalalignment='center', fontdict={'size':14})
+                    ax.text(3+0.05, p2, c + ', ' + str(round(p2)), horizontalalignment='left', verticalalignment='center', fontdict={'size':14})
+                ax.text(1-0.05, 13000, 'BEFORE', horizontalalignment='right', verticalalignment='center', fontdict={'size':18, 'weight':700})
+                ax.text(3+0.05, 13000, 'AFTER', horizontalalignment='left', verticalalignment='center', fontdict={'size':18, 'weight':700})
+                                                                            
+                ax.set_title("Slopechart: Comparing student grades between first and second semester", fontdict={'size':22})
+                ax.set(xlim=(0,4), ylim=(0,14000), ylabel='Mean Grade')
+                ax.set_xticks([1,3])
+                ax.set_xticklabels(["First Semester", "Second Semester"])
+                plt.yticks(np.arange(500, 13000, 2000), fontsize=12)
+                plt.gca().spines["top"].set_alpha(.0)
+                plt.gca().spines["bottom"].set_alpha(.0)
+                plt.gca().spines["right"].set_alpha(.0)
+                plt.gca().spines["left"].set_alpha(.0)
                 st.write(fig)
-     
+                                                                            
             # REPORT
         if mode == 'Report 🖋️':
             st.sidebar.title("Mode 5: Report")
