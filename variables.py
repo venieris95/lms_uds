67aa401ab92bf5e1ef3820fac51399d705133971
rospi <62301694+venieris95@users.noreply.github.com>
2023-01-31 23:11:40 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -8,11 +8,7 @@ def check_credentials():
     """Returns `True` if the user had a correct password."""
     def credentials_entered():
         """Checks whether a password entered by the user is correct."""
-        if (
-            st.session_state["username"] in st.secrets["credentials"]
-            and st.session_state["password"]
-            == st.secrets["credentials"][st.session_state["username"]]
-        ):
+        if (st.session_state["username"] in st.secrets["username"] and st.session_state["password"] == st.secrets["password"][st.session_state["username"]]):
             st.session_state["credentials_correct"] = True
             del st.session_state["password"]  # don't store username + password
             del st.session_state["username"]
@@ -23,17 +19,14 @@ def check_credentials():
         # First run, show inputs for username + password.
         st.warning("Credentials: username: edutechatpython.com | password: WeLo3593!!")
         st.text_input("Username", on_change=credentials_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=credentials_entered, key="password"
-        )
+        st.text_input("Password", type="password", on_change=credentials_entered, key="password")
         return False
     elif not st.session_state["credentials_correct"]:
         # Password not correct, show input + error.
         st.warning("Credentials: username: edutechatpython.com | password: WeLo3593!!")
         st.text_input("Username", on_change=credentials_entered, key="username")
         st.text_input(
-            "Password", type="password", on_change=credentials_entered, key="password"
-        )
+            "Password", type="password", on_change=credentials_entered, key="password")
         st.error("😕 User not known or password incorrect")
         return False
     else:
