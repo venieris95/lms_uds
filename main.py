41d18d641fdb6146823ecda9ad6209e77aeea3bd
rospi <62301694+venieris95@users.noreply.github.com>
2023-02-02 15:05:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ def main():
                 df2 = v.second_semester
                 fig = plt.figure()
                 ax = fig.add_subplot()
-                ax.plot(x = v.overall_1, y = df1["absence"], s=10, c='b', marker="s", label='first_semester')
-                ax.plot(x = v.overall_2, y = df2["absence"], s=10, c='r', marker="o", label='second_semester')
+                ax.plot(v.overall_1, df1["absence"], s=10, c='b', marker="s", label='first_semester')
+                ax.plot(v.overall_2, df2["absence"], s=10, c='r', marker="o", label='second_semester')
                 plt.legend(loc='upper right')
                 ax.set_xlabel("Overall grade")
                 ax.set_ylabel("Absences")
