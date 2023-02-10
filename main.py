d88bb1bf10dc745cd0da3446e5371a1b53406e4a
rospi <62301694+venieris95@users.noreply.github.com>
2023-02-10 12:01:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
                 st.dataframe(v.courses_std_df)
             # VISUALS
         if mode == 'Visuals 📈':
-            graphs = st.sidebar.selectbox("", ('Bar Graph', 'Correlation Heatmap', 'Line Graph', 'Scatter Matrix'))
+            graphs = st.sidebar.selectbox("", ('Bar Graph', 'Correlation Heatmap', 'Scatter Plot', 'Scatter Matrix'))
             if graphs == 'Bar Graph':
                 tab1, tab2 = st.tabs(['First Semester', 'Second Semester'])
                 with tab1:
