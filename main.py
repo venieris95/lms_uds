19273578fa9c231b1aedc013d9d9524524802413
rospi <62301694+venieris95@users.noreply.github.com>
2023-02-10 11:37:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
                     sns.heatmap(df2.corr(), ax=ax)
                     st.write(fig)
             if graphs == 'Line Graph':
-                fig, ax = plt.scatter(v.absence_1, v.overall_1)
+                fig, ax = plt.subplots(v.absence_1, v.overall_1)
                 plt.title("Test")
                 plt.xlabel('Absences')
                 plt.ylabel('Overall Grade')
